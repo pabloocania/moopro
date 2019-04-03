9a7a797d8673a42033cd30db1fc65edf04b451eb
Pablo Ocaña <pabloocana@MacBook-Pro-de-Pablo.local>
2019-04-03 17:36:34 +1300

    fix index.html

--- fullstack/src/client/index.js
+++ fullstack/src/client/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
+import {
+  Router, Route, Switch, Redirect
+} from "react-router-dom";
 
 // core components
 import Admin from "./layouts/Admin";
